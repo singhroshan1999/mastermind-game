96c8434dac8848fcfa592a64897b9ec1ac2f75c9
Roshan Singh <singhroshan1999@gmail.com>
2018-12-31 00:42:12 +0530

    first commit

---
+++ precache-manifest.9ffec79d0125e3db862ad88f8a4a065d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e773b1da7991b769fbd",
+    "url": "/mastermind-game/static/js/runtime~main.2e773b1d.js"
+  },
+  {
+    "revision": "c50b07927156685db265",
+    "url": "/mastermind-game/static/js/main.c50b0792.chunk.js"
+  },
+  {
+    "revision": "f4c894487bd22d11b8ce",
+    "url": "/mastermind-game/static/js/1.f4c89448.chunk.js"
+  },
+  {
+    "revision": "c50b07927156685db265",
+    "url": "/mastermind-game/static/css/main.aff55544.chunk.css"
+  },
+  {
+    "revision": "a840979f598037d7f4f3eaefb55612fa",
+    "url": "/mastermind-game/index.html"
+  }
+];
